2defea090d3a359b12b1e4b2c41a28fde8a5adbd
olehb <legowspace@gmail.com>
2022-09-16 15:45:05 +0300

    Finished hw

--- src/ua/hillelit/homeworks/main/ArrayValueCalculator.java
+++ src/ua/hillelit/homeworks/main/ArrayValueCalculator.java
@@ -5,6 +5,18 @@ import ua.hillelit.homeworks.exceptions.ArraySizeException;
 
 public class ArrayValueCalculator {
     public static int doCalc(String arr[][]) throws ArraySizeException {
+        arraySizeValidator(arr);
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                nullValidator(arr[i][j]);
+                sum = arrayDataValidator(arr[i][j], sum, i, j);
+            }
+        }
+        return sum;
+    }
+
+    private static void arraySizeValidator(String arr[][]) {
         if (arr.length != 4) {
             throw new ArraySizeException("Неверный размер массива!");
         } else {
@@ -14,21 +26,22 @@ public class ArrayValueCalculator {
                 }
             }
         }
+    }
 
-        int sum = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (arr[i][j] == null) {
-                    System.out.println("Элемент не может быть null");
-                    return -1;
-                }
-                sum += Integer.parseInt(arr[i][j]);
-                /*throw new ArrayDataException("Неверный формат в ячейке: ", i, j);
-                }*/
-            }
-            return sum;
+    private static void nullValidator(String arr) {
+        if (arr == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    private static int arrayDataValidator(String arr, int sum, int i, int j) {
+        try {
+            sum += Integer.parseInt(arr);
+        } catch (NumberFormatException e){
+            throw new ArrayDataException("Неверный формат в ячейке: ", i, j);
         }
         return sum;
     }
+
 }
 
--- src/ua/hillelit/homeworks/main/Main.java
+++ src/ua/hillelit/homeworks/main/Main.java
@@ -6,15 +6,18 @@ import ua.hillelit.homeworks.exceptions.ArraySizeException;
 public class Main {
     public static void main(String[] args) {
         String[][] arrStr = {
-            {"4", "5", "6", "7"},
+            {"4", "5", "j", "7"},
             {"1", "2", "3", "8"},
-            {"11", "22", "44", "53"},
+            {"11", "22", "33", "53"},
             {"-10", "-11", "-12", "-13"}};
         try {
             System.out.println(ArrayValueCalculator.doCalc(arrStr));
         }
         catch (ArraySizeException e) {
             System.out.println(e.getMessage());
+        }
+        catch (NullPointerException e) {
+            System.out.println("Элемент не может быть " + e.getMessage());
         }
         catch (ArrayDataException e) {
             System.out.println(e.getMessage() + "[" +e.getRow() + "] " + "[" +e.getColumn() + "]");
